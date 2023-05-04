20f65bf63d59d2a5c9c8921469885743417ff53f
Bruno Queguiner <bruno.queguiner@seenovate.com>
2023-05-05 00:16:49 +0200

    :sparkles: Improvments on the map

--- app/app.py
+++ app/app.py
@@ -1,13 +1,11 @@
 import folium
 import streamlit as st
-from folium.plugins import Draw
 from streamlit_folium import st_folium
-import pandas as pd
 import json
 
 from config import logger
 from header import set_page_config
-from load_data import get_data, get_region, get_indicator, filter_df
+from load_data import get_data, get_region, get_indicator, filter_df, get_dates, get_value_bins
 
 logger.debug("New run of app.py")
 set_page_config()
@@ -16,50 +14,64 @@ DB_CONN = st.experimental_connection('snowpark', type='sql',
 
 
 def main() -> None:
-    st.title("Welcome to the awesome app")
-    tab1, tab2 = st.tabs(["Line Chart", "Map"])
-    with tab1:
-        df = get_data(DB_CONN)
-        region_name = get_region(df)
-        region_id = get_region(df, type="id")
-        indicator_name = get_indicator(df)
-        selected_states = st.multiselect("States", region_name, ['New York', 'Nevada'])
-        selected_indicator = st.selectbox("Indicator", indicator_name)
 
-        df_filter = filter_df(df, selected_states, selected_indicator)
+    # GET SOURCE DATA
+    df = get_data(DB_CONN)
+    region_name = get_region(df)
+    indicator_name = get_indicator(df)
+    dates = get_dates(df)
+    default_date = max(dates)
+
+    st.title("Welcome to the awesome app")
+    tab1, tab2, tab3 = st.tabs(["📈 Line Chart", "🌎 Map", "❓ About"])
+    with tab1:
+        selected_states = st.multiselect("States", region_name, ['New York', 'Nevada'])
+        selected_indicator_chart = st.selectbox("Indicator", indicator_name, key='indicator')
+
+        df_filter = filter_df(df, selected_states, selected_indicator_chart)
         st.line_chart(df_filter, y=selected_states)
 
     with tab2:
-        # # Création d'un DataFrame avec les valeurs pour chaque État
-        # data = {'state': ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA',
-        #                   'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-        #                   'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT',
-        #                   'VA', 'WA', 'WV', 'WI', 'WY'],
-        #         'value': [5, 3, 10, 7, 25, 14, 8, 2, 18, 12, 1, 2, 13, 8, 4, 5, 6, 9, 3, 7, 12, 15, 7, 4, 8, 1, 3, 5, 2,
-        #                   9, 20, 14, 3, 12, 7, 6, 15, 3, 8, 1, 2, 9, 4, 11, 6, 13, 8, 5, 7, 10]}
-        # df = pd.DataFrame(data)
-
+        selected_indicator_map = st.selectbox("Indicator", indicator_name, key='indicator_map')
+        selected_date_map = st.select_slider("Date", dates, value=default_date)
         # Création de la carte
-        m = folium.Map(location=[37, -102], zoom_start=4)
+        m = folium.Map(location=[40, -95], zoom_start=4)
 
         us_states_json = json.load(open('../data/us-states.json'))
         # Ajout de la couche GeoJSON pour les États-Unis
         folium.GeoJson(us_states_json, name='geojson').add_to(m)
 
-        folium.Choropleth(
-            geo_data=us_states_json,  # json
+        df_map = df.loc[(df['Indicator Name'] == selected_indicator_map) & (df['Date'] == selected_date_map)]
+        cp = folium.Choropleth(
+            geo_data=us_states_json,
             name='choropleth',
-            data=df,
-            columns=['Region Name', 'Value'],  # columns to work on
+            data=df_map,
+            columns=['Region Name', 'Value'],
             key_on='feature.properties.name',
-            fill_color='YlGnBu',  # I passed colors Yellow,Green,Blue
+            bins=get_value_bins(df, selected_indicator_map),
+            fill_color='YlOrRd',
             fill_opacity=0.7,
             line_opacity=0.2,
-            legend_name="Real Estate KPI"
+            legend_name=selected_indicator_map + " ($)"
         ).add_to(m)
 
-        # Affichage de la carte
-        st_folium(m)
+        # creating a state indexed version of the dataframe so we can lookup values
+        state_data_indexed = df_map.set_index('Region Name')
+
+        # looping thru the geojson object and adding a new property(unemployment)
+        # and assigning a value from our dataframe
+        for s in cp.geojson.data['features']:
+            try:
+                s['properties']['Value'] = int(state_data_indexed.loc[s['properties']['name'], 'Value'])
+            except KeyError:
+                s['properties']['Value'] = 'No data'
+
+        # and finally adding a tooltip/hover to the choropleth's geojson
+        folium.GeoJsonTooltip(['name', 'Value'], labels=False).add_to(cp.geojson)
+
+        folium.LayerControl().add_to(m)
+        # Display the map
+        st_folium(m, width=1000)
 
 
 if __name__ == '__main__':
--- app/load_data.py
+++ app/load_data.py
@@ -1,33 +1,54 @@
 import pandas as pd
 import streamlit as st
 from streamlit.connections.sql_connection import SQLConnection
-from datetime import timedelta
+from datetime import timedelta, date
 
 
 @st.cache_data(ttl=timedelta(weeks=1), show_spinner="Loading data from Snowpark DB...")
 def get_data(_conn: SQLConnection) -> pd.DataFrame:
-    df = _conn.query("""SELECT * FROM ZRHVI2020JUL WHERE "RegionType" = 'State' and "Region Name" != 'District of Columbia' """, )
+    df = _conn.query("""SELECT * FROM ZRHVI2020JUL 
+                        WHERE "RegionType" = 'State' AND "Region Name" != 'District of Columbia' """)
+    df["year_datetime"] = pd.to_datetime(df["Date"])
+    df["year"] = df["year_datetime"].dt.year
     return df
 
 
 @st.cache_data(ttl=timedelta(weeks=1))
-def get_region(df: pd.DataFrame, type: str = 'name') -> list[str]:
-    if type == 'name':
+def get_region(df: pd.DataFrame, output_type: str = 'name') -> list[str]:
+    if output_type == 'name':
         return list(df["Region Name"].unique())
-    elif type == 'id':
+    elif output_type == 'id':
         return list(df["RegionId"].unique())
 
 
 @st.cache_data(ttl=timedelta(weeks=1))
-def get_indicator(df: pd.DataFrame, type: str = 'name') -> list[str]:
-    if type == 'name':
+def get_indicator(df: pd.DataFrame, output_type: str = 'name') -> list[str]:
+    if output_type == 'name':
         return list(df["Indicator Name"].unique())
-    elif type == 'id':
+    elif output_type == 'id':
         return list(df["Indicator"].unique())
 
 
+@st.cache_data(ttl=timedelta(weeks=1))
 def filter_df(df: pd.DataFrame, state_list: list[str], indicator: str):
-    filter = (df['Indicator Name'] == indicator) & (df['Region Name'].isin(state_list))
-    df_filter = df.loc[filter, ['Date', 'Region Name', 'Value']].drop_duplicates()
-    df_pivot = df_filter.pivot(columns='Region Name', values='Value', index='Date')
+    df_filtered = df.loc[(df['Indicator Name'] == indicator) & (df['Region Name'].isin(state_list)),
+                         ['Date', 'Region Name', 'Value']].drop_duplicates()
+    df_pivot = df_filtered.pivot(columns='Region Name', values='Value', index='Date')
     return df_pivot
+
+
+@st.cache_data(ttl=timedelta(weeks=1))
+def get_dates(df: pd.DataFrame) -> list[date]:
+    return list(sorted(df["Date"].unique()))
+
+
+@st.cache_data(ttl=timedelta(weeks=1))
+def get_value_bins(df: pd.DataFrame, indicator_name: str, nb_bins: int = 15) -> list[int]:
+    min_value = int(min(df.loc[df['Indicator Name'] == indicator_name, 'Value']))
+    max_value = int(max(df.loc[df['Indicator Name'] == indicator_name, 'Value'])) + 1
+    value_range = max_value - min_value
+    value_step = int(value_range / (nb_bins - 1)) + 1
+    value_list = [min_value]
+    for i in range(1, nb_bins):
+        value_list.append(min_value + i * value_step)
+    return value_list
